00569a9dbdf8a2f5d2d3189ab61215f96a423a96
Alexey <lbonanzza@gmail.com>
2019-08-04 22:15:58 +0300

    Initial commit

---
+++ Weight Konvertor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Weight Konvertor
+//
+//  Created by alekseykolesnik on 06/07/2019.
+//  Copyright © 2019 alekseykolesnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var killoText: UITextField!
+    @IBOutlet weak var funtText: UITextField!
+
+    @IBAction func convert(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func `switch`(_ sender: UISwitch) {
+        
+    }
+    
+}
+
